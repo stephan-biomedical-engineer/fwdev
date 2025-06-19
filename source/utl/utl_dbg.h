3e74e216c87708848738c58acf72a1d1d5f7da11
Marcelo Barros de Almeida <marcelo.barros@ufu.br>
2025-06-19 08:28:33 -0300

    adding option for disabling all logs

--- source/utl/utl_dbg.h
+++ source/utl/utl_dbg.h
@@ -26,6 +26,19 @@ extern "C"
     const uint8_t* utl_dbg_mod_name_get(utl_dbg_modules_t mod_idx);
     const char* utl_dbg_base_name_get(const char* full_path);
 
+#ifdef UTL_DBG_DISABLED
+
+#define UTL_DBG_PRINTF(mod, fmt, ...) \
+    do                                \
+    {                                 \
+    } while(0)
+#define UTL_DBG_DUMP(mod, data, size) \
+    do                                \
+    {                                 \
+    } while(0)
+
+#else
+
 #define UTL_LOG_HEADER(mod, fmt, file, line) "[%s][%s:%d] " fmt, (char*) utl_dbg_mod_name_get(mod), file, line
 
 #define UTL_DBG_PRINTF(mod, fmt, ...)                                                                   \
@@ -41,6 +54,8 @@ extern "C"
         if(utl_dbg_mod_enabled(mod))      \
             utl_dbg_dump("", data, size); \
     } while(0)
+
+#endif
 
 #ifdef __cplusplus
 }
