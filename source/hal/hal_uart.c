0dbce04f6cc70421bf88797e68df44b7064d0d34
Marcelo Barros de Almeida <marcelo.barros@ufu.br>
2025-07-06 11:07:12 -0300

    updating driver api

--- source/hal/hal_uart.c
+++ source/hal/hal_uart.c
@@ -1,6 +1,6 @@
 #include "hal.h"
 
-static hal_uart_dev_t *drv = &HAL_UART_DRIVER;
+static hal_uart_driver_t* drv = &HAL_UART_DRIVER;
 
 void hal_uart_init(void)
 {
@@ -12,29 +12,9 @@ void hal_uart_deinit(void)
     drv->deinit();
 }
 
-hal_uart_dev_t hal_uart_create(hal_uart_port_t dev)
+hal_uart_dev_t hal_uart_open(hal_uart_port_t dev, hal_uart_config_t* cfg)
 {
-    return drv->create(dev);
-}
-
-void hal_uart_delete(hal_uart_port_t dev)
-{
-    drv->delete(dev);
-}
-
-bool hal_uart_configure(hal_uart_dev_t dev, hal_uart_config_t* cfg)
-{
-    return drv->configure(dev, cfg);
-}
-
-void hal_uart_interrupt_set(hal_uart_dev_t dev, hal_uart_interrupt_t fun)
-{
-    drv->interrupt_set(dev, fun);
-}
-
-void hal_uart_open(hal_uart_dev_t dev)
-{
-    drv->open(dev);
+    return drv->open(dev, cfg);
 }
 
 void hal_uart_close(hal_uart_dev_t dev)
@@ -52,9 +32,9 @@ int32_t hal_uart_read(hal_uart_dev_t dev, uint8_t* buffer, size_t size)
     return drv->read(dev, buffer, size);
 }
 
-int32_t hal_uart_write(hal_uart_dev_t dev, uint8_t* buffer, size_t len)
+int32_t hal_uart_write(hal_uart_dev_t dev, uint8_t* buffer, size_t size)
 {
-    return drv->write(dev, buffer, len);
+    return drv->write(dev, buffer, size);
 }
 
 void hal_uart_flush(hal_uart_dev_t dev)
@@ -64,10 +44,7 @@ void hal_uart_flush(hal_uart_dev_t dev)
 
 int32_t hal_uart_byte_read(hal_uart_dev_t dev, uint8_t* c)
 {
-    if (drv->bytes_available(dev) > 0)
-        return drv->read(dev, c, 1);
-    else
-        return -1;
+    return drv->read(dev, c, 1);
 }
 
 int32_t hal_uart_byte_write(hal_uart_dev_t dev, uint8_t c)
--- source/hal/hal_uart.h
+++ source/hal/hal_uart.h
@@ -21,6 +21,12 @@ typedef enum hal_uart_baud_rate_e
     HAL_UART_BAUD_RATE_115200,
 } hal_uart_baud_rate_t;
 
+typedef enum hal_uart_stop_bits_e
+{
+    HAL_UART_STOP_BITS_1 = 0,
+    HAL_UART_STOP_BITS_2,
+} hal_uart_stop_bits_t;
+
 typedef enum hal_uart_parity_e
 {
     HAL_UART_PARITY_NONE = 0,
@@ -34,7 +40,7 @@ typedef enum hal_uart_flow_control_e
     HAL_UART_FLOW_CONTROL_CTS_RTS,
 } hal_uart_flow_control_t;
 
-typedef struct* hal_uart_dev_s hal_uart_dev_t;
+typedef struct hal_uart_dev_s* hal_uart_dev_t;
 
 typedef void (*hal_uart_interrupt_t)(uint8_t c);
 
@@ -44,35 +50,28 @@ typedef struct hal_uart_config_s
     hal_uart_parity_t parity;
     hal_uart_stop_bits_t stop_bits;
     hal_uart_flow_control_t flow_control;
+    hal_uart_interrupt_t interrupt_callback;
 } hal_uart_config_t;
 
 typedef struct hal_uart_driver_s
 {
     void (*init)(void);
     void (*deinit)(void);
-    hal_uart_dev_t (*create)(hal_uart_port_t dev);
-    void (*delete)(hal_uart_port_t dev);
-    bool (*configure)(hal_uart_dev_t dev, hal_uart_config_t* cfg);
-    void (*interrupt_set)(hal_uart_dev_t dev, hal_uart_interrupt_t fun);
-    void (*open)(hal_uart_dev_t dev);
+    hal_uart_dev_t (*open)(hal_uart_port_t dev, hal_uart_config_t* cfg);
     void (*close)(hal_uart_dev_t dev);
     size_t (*bytes_available)(hal_uart_dev_t dev);
     int32_t (*read)(hal_uart_dev_t dev, uint8_t* buffer, size_t size);
-    int32_t (*write)(hal_uart_dev_t dev, uint8_t* buffer, size_t len);
+    int32_t (*write)(hal_uart_dev_t dev, uint8_t* buffer, size_t size);
     void (*flush)(hal_uart_dev_t dev);
 } hal_uart_driver_t;
 
 void hal_uart_init(void);
 void hal_uart_deinit(void);
-hal_uart_dev_t hal_uart_create(hal_uart_port_t dev);
-void hal_uart_delete(hal_uart_port_t dev);
-bool hal_uart_configure(hal_uart_dev_t dev, hal_uart_config_t* cfg);
-void hal_uart_interrupt_set(hal_uart_dev_t dev, hal_uart_interrupt_t fun);
-void hal_uart_open(hal_uart_dev_t dev);
+hal_uart_dev_t hal_uart_open(hal_uart_port_t dev, hal_uart_config_t* cfg);
 void hal_uart_close(hal_uart_dev_t dev);
 size_t hal_uart_bytes_available(hal_uart_dev_t dev);
 int32_t hal_uart_read(hal_uart_dev_t dev, uint8_t* buffer, size_t size);
-int32_t hal_uart_write(hal_uart_dev_t dev, uint8_t* buffer, size_t len);
+int32_t hal_uart_write(hal_uart_dev_t dev, uint8_t* buffer, size_t size);
 void hal_uart_flush(hal_uart_dev_t dev);
 int32_t hal_uart_byte_read(hal_uart_dev_t dev, uint8_t* c);
 int32_t hal_uart_byte_write(hal_uart_dev_t dev, uint8_t c);
