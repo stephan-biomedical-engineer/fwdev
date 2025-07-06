fb0bc26b583eb13a7f00b850c47e3206093832e5
Marcelo Barros de Almeida <marcelo.barros@ufu.br>
2025-07-06 11:06:58 -0300

    adding uart driver init/deinit

--- source/hal/hal.c
+++ source/hal/hal.c
@@ -2,6 +2,7 @@
 
 void hal_deinit(void)
 {
+    hal_uart_deinit();
     hal_cpu_deinit();
 }
 
@@ -10,6 +11,7 @@ void hal_init(void)
     utl_dbg_init();
     utl_dbg_mod_enable(UTL_DBG_MOD_PORT);
     hal_cpu_init();
+    hal_uart_init();
 
     // init C random seed
     srand(hal_cpu_random_seed_get());
